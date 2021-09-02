27d48927b01a5f7324b52edcd66710cfe1d18a2b
seamuz <89967885+seamuz@users.noreply.github.com>
2021-09-02 20:09:01 +1000

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-set -e
 
-echo -n "Enter the version you want to upgrade to (Example: 1.14.0) without space: "
-read VERSION
+read -p "Enter the version you want to upgrade to (Example: 1.14.0) without space: " VERSION
+
+set -e
 
 #VERSION="${1?Version must be provided as the first argument to this script}"
 WORKDIR="$(mktemp -d || exit 1)"
 trap 'rm -rf ${WORKDIR}' EXIT
 TAILSCALE_TGZ="${WORKDIR}/tailscale.tgz"
 
-echo "Installing Tailscale in /mnt/data/tailscale"
+echo "Updating Tailscale to version ${VERSION} in /mnt/data/tailscale"
 curl -sSL -o "${TAILSCALE_TGZ}" "https://pkgs.tailscale.com/stable/tailscale_${VERSION}_arm64.tgz"
 tar xzf "${TAILSCALE_TGZ}" -C "${WORKDIR}"
 mkdir -p /mnt/data/tailscale
